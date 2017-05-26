f76116069cbafee32e27e4aa88283c1e41fdd041
Kamesh Sampath <kamesh.sampath@hotmail.com>
2017-05-26 19:42:08 +0530

    Added toast for added item to cart

--- src/main/resources/static/js/moviestore.main.js
+++ src/main/resources/static/js/moviestore.main.js
@@ -1,13 +1,19 @@
 $(document).ready(function () {
 
+    $("#added-to-cart-info").toggleClass("hidden"); //making sure its not shown by default
+
+    $('button.close').on('click', function () {
+        $("#added-to-cart-info").toggleClass("hidden");
+    })
+
     $(".add-to-cart").click(function (event) {
-        event.preventDefault()
+        event.preventDefault();
         addToCart(this.getAttribute('data'));
     });
 
     function addToCart(movieId) {
         var qty = document.getElementById(movieId + '--qty').value;
-        console.log("Adding " + qty + " to movie " + movieId + " in cart");
+        console.log("Adding " + qty + " to movie " + movieId + " to cart");
         $.ajax({
             type: "GET",
             url: '/cart/add?movieId=' + movieId + "&quantity=" + qty,
@@ -15,6 +21,7 @@ $(document).ready(function () {
             success: function (data) {
                 console.log("Success : ", data);
                 $("#cart-items-indicator").text(data);
+                $("#added-to-cart-info").toggleClass("hidden");
             },
             error: function (e) {
                 console.log("ERROR : ", e);
